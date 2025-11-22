703fc19a8b3e3977229857aa81201bd89e710d76
Paul Rose <ptrose78@gmail.com>
2025-11-22 10:09:53 -0600

    Update src site for Turnstile loading in Layout Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Script 
-          src="https://challenges.cloudflare.com/turnstile/v0/api.js" 
+          src="https://challenges.cloudflare.com/turnstile/v0/api.js?onload=turnstileLoaded"
           strategy="lazyOnload" 
         />
         <Navbar />
